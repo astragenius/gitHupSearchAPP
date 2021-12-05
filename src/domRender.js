298a87aa5c87b33517e49bdf6f1864806c14dc4d
astragenius <darthmalak86@gmail.com>
2021-12-05 13:50:36 +0100

    added setUrl function to set the correct link and name to the accoundName section

--- src/domRender.js
+++ src/domRender.js
@@ -1,18 +1,19 @@
 
 
 const renderData = (data) => {
-    const { avatar_url, bio, company, created_at, followers, following, location, login, public_repos, twitter_username, blog, name } = data;
+    const { avatar_url, bio, company, created_at, followers, following, location, login, public_repos, twitter_username, blog, name, html_url } = data;
 
     console.log(data);
     console.log(blog);
     document.getElementById('avatar').src = avatar_url;
-    document.getElementById('bio').innerText = bioCheck(bio);
+    document.getElementById('bio').innerText = checkBio(bio);
     document.getElementById('date').innerText = formatDate(created_at);
-    document.getElementById('login').innerText = login;
+    
     document.getElementById('name').innerText = name;
     document.getElementById('repo').innerText = public_repos;
     document.getElementById('follower').innerText = followers;
     document.getElementById('following').innerText = following;
+    seturl(document.getElementById('login'), html_url, login);
     checkLocation(document.getElementById('location'), location);
     checkTwitter(document.getElementById('twitter'), twitter_username);
     checkLinks(document.getElementById('company'), company);
@@ -23,6 +24,11 @@ const renderData = (data) => {
     
 }   
 
+const seturl = (el, data, dataName) => {
+
+    el.innerText = dataName;
+    el.href = data;
+}
 
 const formatDate = (date) => {
 
@@ -34,7 +40,7 @@ const formatDate = (date) => {
     return `Joined ${day} ${month} ${year}`;     
 }
 
-const bioCheck = (data) => {
+const checkBio = (data) => {
 
     if(data == "" || data == null) {
 
